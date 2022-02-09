577ec41df797feb12c08355abb52cd6fe5ddb551
Mureed Hussain <imhussain0086@gmail.com>
2022-02-09 20:12:27 +0500

    study new
    study new

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Map from "./Map";
+import NewStudy from "./NewStudy";
+
+const App = () => {
+  return (
+    <div>
+      <NewStudy />
+      <Map />
+    </div>
+  );
+};
+
+export default App;
